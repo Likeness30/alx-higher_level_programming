3adcb4e83af30efbfb289c4f92ffca1f475a9997
Likeness30 <likeness14@gmail.com>
2024-01-23 15:02:41 -0800

    102-square.py

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,12 +1,52 @@
 #!/usr/bin/python3
-def magic_calculation(a, b):
-    result = 0
-    for i in range(1, 3):
-        try:
-            if i > a:
-                raise Exception('Too far')
-            result += a ** b / i
-        except Exception:
-            result = b + a
-            break
-    return result
+"""Square class defination"""
+
+
+class Square:
+    """square body"""
+
+    def __init__(self, size=0):
+        """square contructor 
+        Args: size: length of a side of Square
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """"The propery of size as the length 
+        of a side of Square
+        Raises:
+            TypeError: if size != int
+            ValueErrorr: if size < 0
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def area(self):
+        """Get the area instance to comparators"""
+        return self.__size * self.__size
+
+    def __le__(self, other):
+        return self.area() <= other.area()
+
+    def __lt__(self, other):
+        return self.area() < other.area()
+
+    def __ge__(self, other):
+        return self.area() >= other.area()
+
+    def __ne__(self, other):
+        return self.area() != other.area()
+
+    def __gt__(self, other):
+        return self.area() > other.area()
+
+    def __eq__(self, other):
+        return self.area() == other.area()
