6b606f4184c8609575868bc91ab0dab96f3de952
Rufus <likeness14@gmail.com>
2024-03-12 17:40:20 -0700

    16-no_link.sql

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,6 +1,2 @@
---This lists all records of the table second_table having a name value in my MySQL server
--- Records are ordered by descending score
-SELECT `score`, `name`
-FROM `second_table`
-WHERE `name` != ""
-ORDER BY `score` DESC;
+-- This shows everything except null names.
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
