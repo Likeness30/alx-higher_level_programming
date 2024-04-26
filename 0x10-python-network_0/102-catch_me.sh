32b05d99d1a38615f87cf556b55b1c8a69c973fa
Rufus <likeness14@gmail.com>
2024-04-26 17:43:57 +0100

    102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #This script makes a request to 0.0.0.0:5000/catch_me that gets the message "You got me!"
-curl -sL -X PUT -H "Origin: AlxNigeria" -d "user_id=98" 0.0.0.0:5000/catch_me
+curl -sL -X PUT -H "Origin: AlxSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
